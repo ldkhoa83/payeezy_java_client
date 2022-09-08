5862aa2f8798551787585b4359e9e733f695bec1
ldkhoa83@gmail.com <ldkhoa83@gmail.com>
2022-09-08 12:34:49 -0500

    Revert "Upgrade rest client to up-to-date APIs"
    This reverts commit 337c58937647f088c0b0d41afdd82e05b8a54bb8.

--- src/main/java/com/firstdata/payeezy/models/transaction/Loyalty.java
+++
@@ -1,7 +0,0 @@
-package com.firstdata.payeezy.models.transaction;
-
-/**
- * Created by FA7G14Q on 7/27/2015.
- */
-public class Loyalty extends BaseMembership {
-}
--- src/main/java/com/firstdata/payeezy/models/transaction/TokenType.java
+++
@@ -1,8 +0,0 @@
-package com.firstdata.payeezy.models.transaction;
-
-/**
- * Created by FA7G14Q on 6/14/2015.
- */
-public enum TokenType {
-    FDToken
-}
